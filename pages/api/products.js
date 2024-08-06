e9fd2dc4009a48edaa16a1c39b35baeea72636f9
manas vikas gunjal <manasgunjal67@gmail.com>
2024-08-07 03:57:06 +0530

    hogaya

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,7 +1,7 @@
 import { mongooseConnect } from '@/lib/mongoose';
-import { Product } from '@/models/Product';
+// import { Product } from '@/models/Product';
 
-export default async function handle(req,res) {
+export default async function handle(req,res) {x
     const {method}= req;
 
     await mongooseConnect();
